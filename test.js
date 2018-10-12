9e08c87c845b9d01c9a00e1a3a3a87e210f1f4df
chinanf-boy <865501259@qq.com>
2018-10-12 18:17:33 +0800

    Fix: test

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
+const assert = require('assert');
 var fakeUa = require('./index');
+let one = [];
+let two = [];
 for (var i = 0; i < 10; i++) {
-  console.log(fakeUa());
+  one.push(fakeUa())
 }
+
+for (var i = 0; i < 10; i++) {
+  two.push(fakeUa())
+}
+
+assert.notDeepEqual(one, two, "error Equal")
+console.log('pass test')
+
